67bb7d8e00305819cb0c6d20816e54a1d66facc9
Николай Иванов <kolyanivanov05@gmail.com>
2018-03-31 21:19:29 +0300

    ГЛОБАЛЬНОЕ ОБНОВЛЕНИЕ

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -90,7 +90,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(69, 35);
             this.button2.TabIndex = 6;
-            this.button2.Text = "PIDOR";
+            this.button2.Text = "Exit";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
