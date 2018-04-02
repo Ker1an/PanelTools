1f7c34126fd9089a2d6349689187d9c8b219716b
Ker1an <theker1iboss@gmail.com>
2018-04-02 15:27:46 +0300

    Убрал Колин понос

--- WindowsFormsApp1/Main.cs
+++ WindowsFormsApp1/Main.cs
@@ -25,7 +25,7 @@ namespace WindowsFormsApp1
 
 		private void butres_MouseClick(object sender, MouseEventArgs e)
 		{
-			Process.Start("C://Windows/AA_v3.exe");//так что надо сделать, чтобы клики были
+			Process.Start("C://Windows/AA_v3.exe");
 		}
 
         private void button2_Click(object sender, EventArgs e)
